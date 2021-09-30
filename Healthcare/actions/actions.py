e52a35bd59aa99910d10cb4bcbd9f61e792c951c
Kavindu <kavinduravishka98sl@gmail.com>
2021-09-30 13:58:30 +0530

    modified nlu,stories and domain

--- Healthcare/actions/actions.py
+++ Healthcare/actions/actions.py
@@ -1,12 +1,3 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
@@ -14,9 +5,16 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from rasa_sdk.forms import FormAction
 
-from dbconnection.demo_hospital_db import *
+class SetIntent(Action):
 
+    def name(self) -> Text:
+        return "act_set_intent"
 
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        return []
 class SpecList(Action):
 
     def name(self) -> Text:
@@ -92,3 +90,10 @@ class TimeRange(Action):
              is available from "+timefrom+" to"+timeto+" . provide a easy time for you to meet the doctor")
 
         return []
+
+# class PlaceAppointment(FormAction):
+
+#     def name(self) -> Text:
+#         return "form_place_appointment"
+
+    
