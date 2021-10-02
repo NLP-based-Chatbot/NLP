976bfbaac862ec6bc2ab0cfd362fc37329066473
Kavindu <kavinduravishka98sl@gmail.com>
2021-10-03 03:08:33 +0530

    modified actions.py, queries.py

--- Healthcare/actions/actions.py
+++ Healthcare/actions/actions.py
@@ -9,8 +9,17 @@ import json
 import requests
 import environ
 
+from pathlib import Path
+import os
+
 
 env = environ.Env()
+environ.Env.read_env()
+
+CBOTBACKEND = env('CBOTBACKEND')
+QUERYURL = env('QUERYURL')
+
+POSTURL = "http://" + CBOTBACKEND + QUERYURL
 
 class SetIntent(Action):
 
@@ -31,14 +40,15 @@ class SpecList(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        specs = json.loads()
+        response = requests.post(POSTURL,json={"function":"speclist","data":{}})
+        specs = response.json()
 
         buttons = []
 
         for row in specs:
-            buttons.append({"title":row.spec_name,"payload":"/inform{\"entry_id\": \""+str(row.spec_id)+"\"}"})
+            buttons.append({"title":row["fields"]["spec_name"],"payload":"/inform{\"entry_id\": \""+str(row["pk"])+"\"}"})
 
-        dispatcher.utter_message("Select the specialization you want", buttons)
+        dispatcher.utter_button_message("Select the specialization you want", buttons)
 
         return []
 
@@ -52,22 +62,26 @@ class DoctorList(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         spec_id = tracker.get_slot("entry_id")
+        doctor_list = []
 
-        doctors = []
-
-        if spec_id != "" or spec_id != None:
-            # select doctors has no specialization
-            pass
+        if spec_id != "" and spec_id != None:
+            response = requests.post(POSTURL,json={"function":"doctlist","data":{"spec_id":int(spec_id)}})
+            doctor_list = response.json()
         else:
-            # select doctors with specific specialization
-            pass
+            response = requests.post(POSTURL,json={"function":"doctlist","data":{}})
+            doctor_list = response.json()
 
         buttons = []
 
-        for row in doctors:
-            buttons.append({"title": "Dr."+row.first_name+" "+row.last_name,"payload":"/"+str(row.docthash)})
+        for row in doctor_list:
+            buttons.append({"title": "Dr."+row["fields"]["first_name"]+" "\
+                +row["fields"]["last_name"],"payload":"/inform_docthash{\"docthash\":\""+str(row["fields"]["docthash"])+"\"}"})
 
-        dispatcher.utter_message("List of Doctors", buttons)
+        if len(buttons)==0:
+            dispatcher.utter_message("No doctors are found with the provided specialization")
+            return []
+
+        dispatcher.utter_button_message("List of Doctors", buttons)
 
         return []
 
@@ -84,24 +98,28 @@ class TimeRange(Action):
 
         selecteddoctor = None
 
-        if docthash!= "" or docthash!=None:
-            pass
+        if docthash != "" and docthash != None:
+            response = requests.post(POSTURL,json={"function":"docbyhash","data":{"docthash":docthash}})
+            selecteddoctor = response.json()[0]
+            response = requests.post(POSTURL,json={"function":"docavlbl","data":{"docthash":docthash}})
+            doctors_time = response.json()[0]
         else:
-            #fetch info about the doctor who is selected
-            pass
+            dispatcher.utter_message("Doctor's name is not provided")
+            return []
+            
+        first_name = selecteddoctor["fields"]["first_name"]
+        last_name = selecteddoctor["fields"]["last_name"]
+        timefrom = doctors_time["fields"]["time_from"]
+        timeto = doctors_time["fields"]["time_to"]
 
-        timefrom = selecteddoctor.time_from
-        timeto= selecteddoctor.time_to
-
-        dispatcher.utter_message("Dr."+selecteddoctor.first_name+" "+selecteddoctor.last_name+"\
-             is available from "+timefrom+" to"+timeto+" . provide a easy time for you to meet the doctor")
+        dispatcher.utter_message("Dr. "+ first_name +" "+ last_name + " is available from " + timefrom + " to " + timeto)
 
         return []
 
-class PlaceAppointment(FormAction):
+# class PlaceAppointment(FormAction):
 
-    def name(self) -> Text:
-        return "form_place_appointment"
+#     def name(self) -> Text:
+#         return "form_place_appointment"
 
 
 
