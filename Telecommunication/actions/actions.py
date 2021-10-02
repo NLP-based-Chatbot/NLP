7e5f31e4409f9758efea82942eb3afc88d1c9ec9
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2021-10-02 10:45:44 +0530

    Improve chatbot

--- Telecommunication/actions/actions.py
+++ Telecommunication/actions/actions.py
@@ -3,6 +3,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+ALLOWD_SERVICE_PROVIDERS = ["dialog", "mobitel", "hutch", "sltmobitel", "airtel", "slt"]
 
 class ActionPackageDetails(Action):
 
@@ -16,3 +17,20 @@ class ActionPackageDetails(Action):
         dispatcher.utter_message(text="Hello World!")
 
         return []
+
+class ValidateDataPackageForm(Action):
+
+    def name(self) -> Text:
+        return "validate_data_package_form"
+
+    def validate_service_provider(self, 
+            slot_value: Any,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+       
+        if slot_value.lower() not in ALLOWD_SERVICE_PROVIDERS:
+            dispatcher.utter_message(text=f"We can only give details about Dialog, Mobitel, Hutch and Airtel.")
+            return {"service_provider": None}
+        dispatcher.utter_message(text=f"OK! your provider is {slot_value}.")
+        return {"service_provider": slot_value}
