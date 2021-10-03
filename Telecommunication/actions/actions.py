42e389868920e4cc4ee8ea555ee76764ebd5025b
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2021-10-03 10:15:00 +0530

    improve chatbot

--- Telecommunication/actions/actions.py
+++ Telecommunication/actions/actions.py
@@ -1,11 +1,16 @@
 from typing import Any, Text, Dict, List
-
+import requests
 from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 
+from rasa_sdk.events import (
+  SlotSet,
+  EventType
+)
+
 ALLOWD_SERVICE_PROVIDERS = ["dialog", "mobitel", "hutch", "sltmobitel", "airtel", "slt"]
 PAYMENT_METHODS = ["prepaid", "postpaid"]
-PACKAGE_TYPES = ["data card", "time based", "content based", "anytime", "unlimited"]
+PACKAGE_TYPES = ["data-card", "time-based", "content-based", "anytime", "unlimited"]
 
 class ActionPackageDetails(Action):
 
@@ -16,9 +21,23 @@ class ActionPackageDetails(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Hello World!")
+        service_provider = tracker.get_slot('service_provider')
+        payment_method = tracker.get_slot('payment_method')
+        package_type = tracker.get_slot('package_type')
 
-        return []
+        service_provider = service_provider.lower()
+        payment_method = payment_method.lower()
+        package_type = package_type.lower()
+
+        results = requests.get(f"http://127.0.0.1:8000/packages/{service_provider}/{payment_method}/{package_type}").json()
+        if(results == None):
+            dispatcher.utter_message(text = "Sorry, no entries found.")
+        else:
+            for result in results:
+                dispatcher.utter_message(text = f"Package name: {result['package_name']} | Rs. {result['value']} | Description: {result['description']} | How to Activate: {result['activation_method']}")
+
+        return [SlotSet("service_provider", None), SlotSet("payment_method", None), SlotSet("package_type", None)]     
+
 
 class ValidateDataPackageForm(FormValidationAction):
 
@@ -56,7 +75,76 @@ class ValidateDataPackageForm(FormValidationAction):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
        
         if slot_value.lower() not in PACKAGE_TYPES:
-            dispatcher.utter_message(text=f"We can only give details about Data card, time based, content based, anytime and unlimited plans.")
+            dispatcher.utter_message(text=f"We can only give details about Data-card, time-based, content-based, anytime and unlimited plans.")
             return {"package_type": None}
-        dispatcher.utter_message(text=f"OK! you want details about{slot_value} plans.")
+        dispatcher.utter_message(text=f"OK! you want details about {slot_value} plans.")
         return {"package_type": slot_value}
+
+
+
+
+class SlotResetTravel(Action):
+  """Resets the slots related to transportation"""
+
+  def name(self):
+
+    return "action_reset_transport_slots"
+
+  def run(
+    self,
+    dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: Dict[Text, Any],
+  ) -> List[EventType]:
+
+    return [SlotSet("location_departure", None), SlotSet("location_destination", None), SlotSet("travelling_mode", None)]  
+
+class ActionSchedule(Action):
+  """Retrive transportation schedule"""
+
+  def name(self):
+
+    return "action_get_schedule"
+
+  def run(
+    self,
+    dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: Dict[Text, Any],
+  ) -> List[EventType]:
+    departure = tracker.get_slot('location_departure')
+    destination = tracker.get_slot('location_destination')
+    method = tracker.get_slot('travelling_mode')
+
+    if destination and departure and method:
+      departure = departure.lower()
+      destination = destination.lower()
+
+      results = requests.get(f"http://127.0.0.1:8000/transport/schedule/{method}/{departure}/{destination}").json()
+      if(results == None):
+        dispatcher.utter_message(text = "Sorry, no entries found.")
+      else:
+        for result in results:
+           dispatcher.utter_message(text = f"Vehical Number: {result['vehical_number']} -> Departure: {result['dep_time']} -> Arrival: {result['des_time']}")
+
+    return [SlotSet("location_departure", None), SlotSet("location_destination", None), SlotSet("travelling_mode", None)]     
+
+class ActionTriggerResponseSelector(Action):
+  """Retrival Action Finder"""
+
+  def name(self) -> Text:
+
+    return "action_trigger_response_selector"
+
+  def run(
+    self,
+    dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: Dict[Text, Any],
+  ) -> List[EventType]:
+    retrieval_intent = tracker.get_slot("retrieval_intent")
+    if retrieval_intent:
+      dispatcher.utter_message(template=f"utter_{retrieval_intent}")
+
+    return [SlotSet("retrieval_intent", None)]  
+
